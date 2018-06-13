4e05b7038d35342dcf0613e5fd7cdf18c93d7c27
Leyaim Misael Jimenez Elizondo <ljimenez6@ucol.mx>
2018-06-12 20:54:37 -0500

    c3

--- controllers/team_controller.js
+++ controllers/team_controller.js
@@ -2,6 +2,8 @@ const TeamModel = require('../models/team_model'),
     tm = new TeamModel(); 
 
 class TeamController {
+
+    //Autos
     getAll(req, res, next) {
         tm.getAll((err, data) => {
             if (!err) {
@@ -19,7 +21,7 @@ class TeamController {
             if (!err) {
                 res.render('edit', { 
                     
- data: data 
+                       data: data 
                 });
             }
         })
@@ -65,27 +67,7 @@ class TeamController {
         res.render('add', { title: 'Agregar Contacto' });
     }
 
-    addv2(req, res, next){
-        let contacto = {
-            noSerie:(req.body.noSerie),
-            Marca:req.body.Marca, 
-            Transmision:req.body.Transmision,
-            Puertas:req.body.Puertas,
-            FechaIntroduccion:req.body.FechaIntroduccion
-           
-           
-        };
-
-        console.log(contacto);
-        tm.insert(contacto, (err) => {
-            if (!err) {
-                res.redirect('/');
-            } else {
-                return next(new Error('Registro no salvado'));
-            }
-        });
-    }
-
+   
      index(req,res,next){
         res.render('index');
      }  
@@ -97,6 +79,326 @@ class TeamController {
 
         res.render('error', { error: err });
     }
+    //clientes
+    getAllClientes(req, res, next) {
+        tm.getAllClientes((err, data) => {
+            if (!err) {
+                res.render('clientes/listC', {  data: data }); 
+                
+            }
+        })
+    }
+
+    getOneC(req, res, next) {
+        let noCliente = req.params.noCliente; 
+        console.log(noCliente);
+
+        tm.getOneClientes(noCliente, (err, data) => {
+            if (!err) {
+                res.render('clientes/editC', { 
+                    
+                       data: data 
+                });
+            }
+        })
+    }
+
+      saveC(req, res, next) {
+        let cliente = {
+            noCliente: (req.body.noCliente || 0),
+            aPaterno: req.body.aPaterno, 
+            aMaterno:req.body.aMaterno, 
+            nombres: req.body.nombres,
+            domicilio: req.body.domicilio,
+            FechaNac: req.body.FechaNac,
+            sexo:req.body.sexo,
+            sucursalOrigen:req.body.sucursalOrigen
+           
+           
+        };
+
+        console.log(cliente);
+        tm.saveCliente(cliente, (err) => {
+            if (!err) {
+                res.redirect('clientes/listC');
+            } else {
+                return next(new Error('Registro no salvado'));
+            }
+        });
+    }
+
+    deleteC(req, res, next) {
+
+        let noCliente = req.params.noCliente; 
+        console.log(noCliente)
+
+        tm.deleteCliente(noCliente, (err, data) => {
+            if (!err) {
+                res.redirect('/getAllClientes')
+            } else {
+                return next(new Error('Registro no encontrado'))
+            }
+        });
+    }
+
+    addFormCliente(req, res, next) {
+        res.render('clientes/addC', { title: 'Agregar Contacto' });
+    }
+
+   //sucursales
+
+     getAllSucursales(req, res, next) {
+        tm.getAllSucursal((err, data) => {
+            if (!err) {
+                res.render('Sucursal/listS', {  data: data }); 
+                
+            }
+        })
+    }
+
+    getOneSucuesal(req, res, next) {
+        let noSucursal = req.params.noSucursal; 
+        console.log(noSucursal);
+
+        tm.getOneSucursal(noSucursal, (err, data) => {
+            if (!err) {
+                res.render('Sucursal/editS', { 
+                    
+                       data: data 
+                });
+            }
+        })
+    }
+
+      saveSucursal(req, res, next) {
+        let sucursal = {
+            noSucursal: (req.body.noSucursal || 0),
+            nombre: req.body.nombre, 
+            domicilio:req.body.domicilio 
+        
+        };
+
+        console.log(sucursal);
+        tm.saveSucursal(sucursal, (err) => {
+            if (!err) {
+                res.redirect('/getAllSucursales');
+            } else {
+                return next(new Error('Registro no salvado'));
+            }
+        });
+    }
+
+    deleteSucuesal(req, res, next) {
+
+        let noSucursal = req.params.noSucursal; 
+        console.log(noSerie)
+
+        tm.deleteSucursal(noSucursal, (err, data) => {
+            if (!err) {
+                res.redirect('Sucursal/listS')
+            } else {
+                return next(new Error('Registro no encontrado'))
+            }
+        });
+    }
+
+    addFormSucursal(req, res, next) {
+        res.render('Sucursal/addS', { title: '' });
+    }
+
+   //inventario
+
+   getAllInventario(req, res, next) {
+    tm.getAllInventario((err, data) => {
+        if (!err) {
+            res.render('inventario/listInv', {  data: data }); 
+            
+        }
+    })
+}
+
+getOneInventario(req, res, next) {
+    let idAuto = req.params.idAuto; 
+    console.log(idAuto);
+
+    tm.getOneInventario(idAuto, (err, data) => {
+        if (!err) {
+            res.render('inventario/addInventario', { 
+                
+                   data: data 
+            });
+        }
+    })
+}
+
+  saveInventario(req, res, next) {
+    let inventario = {
+        idAuto: (req.body.idAuto || 0),
+        Auto: req.body.Auto, 
+        Sucursal:req.body.Sucursal,
+        Color:req.body.Color 
+    
+    };
+
+    console.log(inventario );
+    tm.saveInventario(inventario , (err) => {
+        if (!err) {
+            res.redirect('/getAllInventarios');
+        } else {
+            return next(new Error('Registro no salvado'));
+        }
+    });
+}
+
+deleteInventario(req, res, next) {
+
+    let  idAuto = req.params.idAuto; 
+    console.log( idAuto)
+
+    tm.deleteInventario( idAuto, (err, data) => {
+        if (!err) {
+            res.redirect('/getAllInventarios')
+        } else {
+            return next(new Error('Registro no encontrado'))
+        }
+    });
+}
+
+addFormInventario(req, res, next) {
+    res.render('inventario/addInventario', { title: '' });
+}
+
+//trabajadores
+
+getAllTrabajador(req, res, next) {
+    tm.getAllTrabajadores((err, data) => {
+        if (!err) {
+            res.render('trabajador/listInv', {  data: data }); 
+            
+        }
+    })
+}
+
+getOneTrabajador(req, res, next) {
+    let idTrabajador = req.params.idTrabajador; 
+    console.log(idTrabajador);
+
+    tm.getOneTrabajador(idTrabajador, (err, data) => {
+        if (!err) {
+            res.render('inventario/addInventario', { 
+                
+                   data: data 
+            });
+        }
+    })
+}
+
+  saveTrabajador(req, res, next) {
+    let trabajador = {
+        idTrabajador: (req.body.idTrabajador || 0),
+        aPaterno: req.body.aPaterno, 
+        aMaterno:req.bod.aMaterno,
+        nombres:req.body.nombres,
+        tipoTrabajadro:req.body.tipoTrabajadro,
+        sucursal:req.body.sucursal,
+        idSupervisor:req.body.idSupervisor,
+        salario:req.body.salario 
+    
+    };
+
+    console.log(trabajador );
+    tm.saveTrabajador(inventario , (err) => {
+        if (!err) {
+            res.redirect('/getAllInventarios');
+        } else {
+            return next(new Error('Registro no salvado'));
+        }
+    });
+}
+
+deleteTrabajador (req, res, next) {
+
+    let  idTrabajador = req.params.idTrabajador; 
+    console.log(idTrabajador)
+
+    tm.deleteTrabajador(idTrabajador, (err, data) => {
+        if (!err) {
+            res.redirect('/getAllInventarios')
+        } else {
+            return next(new Error('Registro no encontrado'))
+        }
+    });
+}
+
+ addFormTrabajador(req, res, next) {
+    res.render('inventario/addInventario', { title: '' });
+}
+
+//ventas
+
+getAllVentas(req, res, next) {
+    tm.getAllVentas((err, data) => {
+        if (!err) {
+            res.render('trabajador/listInv', {  data: data }); 
+            
+        }
+    })
+}
+
+getOneVentas(req, res, next) {
+    let idVenta = req.params.idVenta; 
+    console.log(idVenta);
+
+    tm.getOneVenta(idVenta, (err, data) => {
+        if (!err) {
+            res.render('inventario/addInventario', { 
+                
+                   data: data 
+            });
+        }
+    })
+}
+
+  saveVentas(req, res, next) {
+    let ventas = {
+        idVenta: (req.body.idVenta || 0),
+        Auto: req.body.Auto, 
+        Sucursal:req.bod.Sucursal,
+        Cliente:req.body.Cliente,
+        fechaVenta:req.body.fechaVenta,
+        Vendedor:req.body.Vendedor
+      
+    
+    };
+
+    console.log(ventas );
+    tm.saveVentas(ventas , (err) => {
+        if (!err) {
+            res.redirect('/getAllInventarios');
+        } else {
+            return next(new Error('Registro no salvado'));
+        }
+    });
+}
+
+deleteVentas (req, res, next) {
+
+    let  ventas = req.params.ventas; 
+    console.log(ventas)
+
+    tm.deleteVentas(ventas, (err, data) => {
+        if (!err) {
+            res.redirect('/getAllInventarios')
+        } else {
+            return next(new Error('Registro no encontrado'))
+        }
+    });
+}
+
+ addFormVentas(req, res, next) {
+    res.render('inventario/addInventario', { title: '' });
+}
+
 }
 
 module.exports = TeamController;
--- models/team_model.js
+++ models/team_model.js
@@ -75,13 +75,136 @@ class TeamModel {
     }
 
     
-
-
     deleteCliente(noCliente, cb) {
         conn.query('DELETE FROM clientes WHERE noCliente = ?', noCliente, cb);
     }
 
+    //sucursales
+    getAllSucursal(cb) { 
+        conn.query('SELECT * FROM sucursales', cb); 
+    }
+
+    
+
+    getOneSucursal(noSucursal, cb) {
+        conn.query('SELECT * FROM sucursales WHERE noSucursal = ?', noSucursal , cb);
+         
+    }
+
+   
+    saveSucursal(data, cb) {
+        conn.query('SELECT * FROM sucursales WHERE noSucursal = ?', data.noSucursal, (err, rows) => {
+           
+
+            if (!err)
+                return (rows.length == 1)
+                    ? conn.query('UPDATE sucursales SET ? WHERE noSucursal = ?', [data, data.noSucursal], cb)
+                    : conn.query('INSERT INTO sucursales SET ?', data,cb);
+                 
+                   
+        });
+    }
+
+    
+
+    
+    deleteSucursal(noSucursal, cb) {
+        conn.query('DELETE FROM sucursales WHERE noSucursal = ?', noSucursal, cb);
+    }
+
+    //inventario
+
+    getAllInventario(cb) { 
+        conn.query('SELECT * FROM inventariosucursal WHERE Sucursal = 4', cb); 
+    }
+
+    
+
+    getOneInventario(idAuto, cb) {
+        conn.query('SELECT * FROM inventariosucursal WHERE idAuto = ?', idAuto , cb);
+         
+    }
+
+   
+    saveInventario(data, cb) {
+        conn.query('SELECT * FROM inventariosucursal WHERE idAuto = ?', data.idAuto, (err, rows) => {
+           
+
+            if (!err)
+                return (rows.length == 1)
+                    ? conn.query('UPDATE inventariosucursal SET ? WHERE idAuto = ?', [data, data.idAuto], cb)
+                    : conn.query('INSERT INTO inventariosucursal SET ?', data,cb);
+                 
+                   
+        });
+    }
+
+    deleteInventario(idAuto, cb) {
+        conn.query('DELETE FROM inventariosucursal WHERE idAuto = ?',idAuto, cb);
+    }
   
+    //Trabajadores
+    getAllTrabajadores(cb) { 
+        conn.query('SELECT * FROM trabajadores ', cb); 
+    }
+
+    
+
+    getOneTrabajador(idTrabajador, cb) {
+        conn.query('SELECT * FROM trabajadores  WHERE idTrabajador = ?', idTrabajador , cb);
+         
+    }
+
+   
+    saveTrabajador(data, cb) {
+        conn.query('SELECT * FROM trabajadores  WHERE idTrabajador = ?', data.idTrabajador, (err, rows) => {
+           
+
+            if (!err)
+                return (rows.length == 1)
+                    ? conn.query('UPDATE trabajadores  SET ? WHERE idTrabajador = ?', [data, data.idTrabajador], cb)
+                    : conn.query('INSERT INTO trabajadores  SET ?', data,cb);
+                 
+                   
+        });
+    }
+
+    deleteTrabajador(idTrabajador, cb) {
+        conn.query('DELETE FROM trabajadores  WHERE idTrabajador = ?',idTrabajador, cb);
+    }
+
+    //ventas
+
+    getAllVentas(cb) { 
+        conn.query('SELECT * FROM ventas ', cb); 
+    }
+
+    
+
+    getOneVenta(idTrabajador, cb) {
+        conn.query('SELECT * FROM ventas WHERE idVenta = ?', idVenta , cb);
+         
+    }
+
+   
+    saveVentas(data, cb) {
+        conn.query('SELECT * FROM ventas  WHERE idVenta = ?', data.idVenta, (err, rows) => {
+           
+
+            if (!err)
+                return (rows.length == 1)
+                    ? conn.query('UPDATE ventas SET ? WHERE idVenta = ?', [data, data.idVenta], cb)
+                    : conn.query('INSERT INTO ventas  SET ?', data,cb);
+                 
+                   
+        });
+    }
+
+    deleteVentas(idVenta, cb) {
+        conn.query('DELETE FROM ventas  WHERE idVenta = ?',idVenta, cb);
+    }
+
+
 }
 
 module.exports = TeamModel;
